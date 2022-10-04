75c5f5668f1fe44f79fc08e50ca0a9ab84a556a4
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-10-04 19:19:42 +0300

    Fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 // import Header from './Header';
 
 function App() {
 
-  
+  const [contacts, setContacts] = useState([])
+  const[addFormData, setAddFormData] = useState({
+    name: "",
+    address: "",
+    phoneNumber: "",
+    email: ""
+  })
+
+  function handleFormChange(e) {
+    e.preventDefault();
+    const fieldName = e.target.getAttribute("name")
+    const fieldValue = e.target.value;
+    const newFormData = {...addFormData}
+    newFormData[fieldValue] = fieldName
+
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/contacts")
+    .then(resp => resp.json())
+    .then(data => setContacts(data))
+  }, [])
   return (
     <div className='app-container'>
       <table>
@@ -17,14 +38,47 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Bob Kline</td>
-            <td>P.O.Box 12123,Nairobi</td>
-            <td>072127373</td>
-            <td>bobkline@gmail.com</td>
-          </tr>
+          {contacts.map((contact) => {
+            return(
+             <tr>
+             <td>{contact.name}</td>
+             <td>{contact.address}</td>
+             <td>{contact.phoneNumber}</td>
+             <td>{contact.email}</td>
+           </tr>
+            )
+          })}
+         
         </tbody>
       </table>
+      <h2>Add Contact</h2>
+      <form>
+        <input 
+        type="text"
+        name='name'
+        required= "required"
+        placeholder='Enter a name...'
+        />
+        <input 
+        type="text"
+        name='address'
+        required= "required"
+        placeholder='Enter an address...'
+        />
+        <input 
+        type="text"
+        name='number'
+        required= "required"
+        placeholder='Enter a number...'
+        />
+         <input 
+        type="text"
+        name='email'
+        required= "required"
+        placeholder='Enter an email...'
+        />
+        <button type='submit'>Add</button>
+      </form>
     </div>
   )
 }
