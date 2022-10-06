219783b5f4f83ba0b543ea065929939fbc136262
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-10-06 11:09:56 +0300

    Navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,68 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+// function NavBar() {
+//     const linkStyles = {
+//         display: "inline-block",
+//         width: "50px",
+//         padding: "12px",
+//         margin: "0 6px 6px",
+//         background: "blue",
+//         textDecoration: "none",
+//         color: "white",
+//       };
+//   return (
+//     <nav className='nav'>
+
+//         <NavLink
+//         to="/"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Home
+//       </NavLink>
+//       {/* <NavLink
+//         to="/about"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         About
+//       </NavLink> */}
+//       <NavLink
+//         to="/add"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Add
+//       </NavLink>
+//     </nav>
+//   )
+// }
+
+// export default NavBar
+
+
 function NavBar() {
-    const linkStyles = {
-        display: "inline-block",
-        width: "50px",
-        padding: "12px",
-        margin: "0 6px 6px",
-        background: "blue",
-        textDecoration: "none",
-        color: "white",
-      };
   return (
-    <div>
-        <NavLink
-        to="/"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/add"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Add
-      </NavLink>
-    </div>
+    <nav className='nav'>
+      <NavLink exact to='/'className='site-title'>Contact App</NavLink>
+      <ul>
+        <li className='active'>
+          <NavLink exact to='/add'>Add</NavLink>
+        </li>
+        <li className='active'>
+          <NavLink exact to='/about'>About</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
